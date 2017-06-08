d43098b0bf0f785187980d784669f34f1c92e93b
Lucas Conti <lconti@qualtrax.com>
2017-06-08 14:30:26 -0400

    begin refactor on game-of-life.js

--- GameOfLifeKata/Scripts/game-of-life.js
+++ GameOfLifeKata/Scripts/game-of-life.js
@@ -1,55 +1,82 @@
 ﻿const AttrRowIndex = 1;
 const AttrColumnIndex = 2;
+const GridRows = 10;
+const GridColumns = 10;
+const NeverAliveCode = -1;
+const AliveCode = 0;
+const NewlyDeadCode = 1;
 
-var cells = new Array(10);
-for (var i = 0; i < cells.length; i++) {
-    cells[i] = new Array(10);
-    for (var j = 0; j < 10; j++) {
-        cells[i][j] = -1;
+var encodedCellGrid;
+
+var getNewEncodedCellsGrid = function () {
+    var cells = new Array(GridRows);
+    for (var row = 0; row < GridRows; row++) {
+        cells[row] = new Array(GridColumns);
+        for (var columns = 0; columns < GridColumns; columns++) {
+            cells[row][columns] = NeverAliveCode;
+        }
     }
+    return cells;
 }
+var encodedCellGrid = getNewEncodedCellsGrid();
 
-$("#do-stuff").on("click", function (event) {
+var postInitialGeneration = function (onSuccess) {
     $.ajax({
         url: 'http://' + window.location.host + '/api/GameOfLife/PostInitialGeneration',
         data: {
-            '': cells
+            '': encodedCellGrid
         },
         method: "POST",
-        success: function (result) {
-            $.ajax({
-                url: 'http://' + window.location.host + '/api/GameOfLife/GetNextGeneration',
-                method: "GET",
-                success: function (result) {
-                    for (var row = 0; row < result.length; row++)
-                        for (var column = 0; column < result[row].length; column++) {
-                            var oldLifeStateCode = cells[row][column];
-                            var oldClass = getLifeStateClass(oldLifeStateCode);
-                            var newLifeStateCode = result[row][column];
-                            var newClass = getLifeStateClass(newLifeStateCode);
-
-                            var cell = $("#cell-" + row + "-" + column);
-                            replaceClass(cell, oldClass, newClass);
-                            cells[row][column] = newLifeStateCode;
-                        }
-                }
-            });
-        }
+        success: onSuccess
     });
-});
+}
+
+var getNextGeneration = function (onSuccess) {
+    $.ajax({
+        url: 'http://' + window.location.host + '/api/GameOfLife/GetNextGeneration',
+        method: "GET",
+        success: updateGrid
+    });
+}
+
+var updateGrid = function (newEncodedCellGrid) {
+    var oldEncodedCellGrid = encodedCellGrid;
+
+    for (var row = 0; row < GridRows; row++) {
+        for (var column = 0; column < GridColumns; column++) {
+            var cell = getCellJQuery(row, column);
+            cell.removeClass(getLifeStateClass(oldEncodedCellGrid[row][column]));
+            cell.addClass(getLifeStateClass(newEncodedCellGrid[row][column]));
+        }
+    }
+
+    encodedCellGrid = newEncodedCellGrid;
+}
+
+var getCellJQuery = function(row, column) {
+    return $("#cell-" + row + "-" + column);
+}
+
+var onStepButtonClick = function (event) {
+    postInitialGeneration(getNextGeneration);
+    
+};
+
+$("#do-stuff").on("click", onStepButtonClick);
 
 $(".cell").on("click", function (event) {
     var attrs = this.id.split('-');
     var row = attrs[AttrRowIndex];
     var column = attrs[AttrColumnIndex];
 
-    var oldLifeStateCode = cells[row][column];
+    var oldLifeStateCode = encodedCellGrid[row][column];
     var oldClass = getLifeStateClass(oldLifeStateCode);
-    var newLifeStateCode = getNextLifeStateCode(cells[row][column]); 
+    var newLifeStateCode = getNextLifeStateCode(encodedCellGrid[row][column]); 
     var newClass = getLifeStateClass(newLifeStateCode);
 
     replaceClass($(this), oldClass, newClass);
-    cells[row][column] = newLifeStateCode;
+    encodedCellGrid[row][column] = newLifeStateCode;
+
 });
 
 var replaceClass = function (cell, oldClass, newClass) {
@@ -58,17 +85,17 @@ var replaceClass = function (cell, oldClass, newClass) {
 }
 
 var getLifeStateClass = function (lifeStateCode) {
-    if (lifeStateCode === 0)
+    if (lifeStateCode === AliveCode)
         return "alive-cell";
-    else if (lifeStateCode === -1)
+    else if (lifeStateCode === NeverAliveCode)
         return "never-alive-cell";
     else
         return "dead-cell";
 }
 
 var getNextLifeStateCode = function (currentLifeStateCode) {
-    if (currentLifeStateCode === 0)
-        return 1;
+    if (currentLifeStateCode === AliveCode)
+        return NewlyDeadCode;
     else
-        return 0;
+        return AliveCode;
 }
